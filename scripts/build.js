c3203b09bbb637c5a38f35050a112817f76f52c9
denys.oblohin <denys.oblohin@okta.com>
2021-09-23 23:08:45 +0300

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,39 @@
 'use strict'
 
-const shell = require('shelljs')
-const chalk = require('chalk')
-const fs = require('fs')
+const shell = require('shelljs');
+const chalk = require('chalk');
+const fs = require('fs');
 
-const NPM_DIR = `dist`
-const BANNER_CMD = `yarn banners`
+const NPM_DIR = `dist`;
+const BANNER_CMD = `yarn banners`;
 
-shell.echo(`Start building...`)
+shell.echo(`Start building...`);
 
-shell.rm(`-Rf`, `${NPM_DIR}/*`)
+shell.rm(`-Rf`, `${NPM_DIR}/*`);
 if (!fs.existsSync(NPM_DIR)){
     fs.mkdirSync(NPM_DIR);
-}
+};
 
 // Maintain banners
 if (shell.exec(BANNER_CMD).code !== 0) {
-  shell.echo(chalk.red(`Error: Maintain banners failed`))
-  shell.exit(1)
-}
+  shell.echo(chalk.red(`Error: Maintain banners failed`));
+  shell.exit(1);
+};
 
-shell.cp(`-Rf`, [`src`, `index.js`, `package.json`, `LICENSE`, `*.md`], `${NPM_DIR}`)
+shell.cp(`-Rf`, [`src`, `index.js`, `package.json`, `LICENSE`, `*.md`], `${NPM_DIR}`);
 
-shell.echo(`Modifying final package.json`)
-const packageJSON = JSON.parse(fs.readFileSync(`./${NPM_DIR}/package.json`))
-delete packageJSON.private
-delete packageJSON.scripts
+shell.echo(`Modifying final package.json`);
+const packageJSON = JSON.parse(fs.readFileSync(`./${NPM_DIR}/package.json`));
+delete packageJSON.private;
+delete packageJSON.scripts;
 
 // Remove "dist/" from the entrypoint paths.
 ['main', 'module', 'types'].forEach(function (key) {
   if (packageJSON[key]) {
-    packageJSON[key] = packageJSON[key].replace('dist/', '')
+    packageJSON[key] = packageJSON[key].replace('dist/', '');
   }
-})
+});
 
-fs.writeFileSync(`./${NPM_DIR}/package.json`, JSON.stringify(packageJSON, null, 4))
+fs.writeFileSync(`./${NPM_DIR}/package.json`, JSON.stringify(packageJSON, null, 4));
 
-shell.echo(chalk.green(`End building`))
+shell.echo(chalk.green(`End building`));
