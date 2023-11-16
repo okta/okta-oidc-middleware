210ebab88c1860836031989ef2f0c51f35109056
Jared Perreault <jared.perreault@okta.com>
2023-11-16 16:51:52 -0500

    adds node 20 support

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -5,6 +5,30 @@ source ${OKTA_HOME}/${REPO}/scripts/setup.sh
 export TEST_SUITE_TYPE="junit"
 export TEST_RESULT_FILE_DIR="${REPO}/reports/unit"
 
+# Run unit tests on multiple node versions
+
+nvm install 14
+# Run jest with "ci" flag
+if ! yarn test:unit --ci; then
+  echo "unit failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+nvm install 16
+# Run jest with "ci" flag
+if ! yarn test:unit --ci; then
+  echo "unit failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+nvm install 18
+# Run jest with "ci" flag
+if ! yarn test:unit --ci; then
+  echo "unit failed! Exiting..."
+  exit ${TEST_FAILURE}
+fi
+
+nvm install 20
 # Run jest with "ci" flag
 if ! yarn test:unit --ci; then
   echo "unit failed! Exiting..."
