8cad18ec1e387db65a39d4bee3b3e30de748917b
Jared Perreault <jared.perreault@okta.com>
2023-11-19 20:52:03 -0500

    test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,15 +5,15 @@ export NVM_DIR="/root/.nvm"
 setup_service node "${1:-v16.16.0}"
 
 # NOTE: designed to run on CentOS
-distro=$(cat /etc/os-release | head -n1)
+distro=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | tr -d '"')
 echo $distro
 
-if [ "$distro" = "CentOS Linux" ]; then
+if [ "$distro" = "centos" ]; then
   # Use the cacert bundled with centos as okta root CA is self-signed and cause issues downloading from yarn
   setup_service yarn 1.21.1 /etc/pki/tls/certs/ca-bundle.crt
   # Add yarn to the $PATH so npm cli commands do not fail
   export PATH="${PATH}:$(yarn global bin)"
-elif [ "$distro" = "Amazon Linux" ]; then
+elif [ "$distro" = "amzn" ]; then
   npm install -g yarn
   export PATH="$PATH:$(npm config get prefix)/bin"
 else
