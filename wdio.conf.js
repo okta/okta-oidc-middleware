9387fe65294f653d8450d5e2860e8844fc74b494
Shaw Yu <shaw.yu@okta.com>
2024-10-14 17:45:00 -0700

    try to save a screenshot

--- wdio.conf.js
+++ wdio.conf.js
@@ -305,8 +305,11 @@ exports.config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+    afterTest: async function(test, context, { passed }) {
+      if (!passed) {
+        await browser.saveScreenshot(`./reports/e2e/${test.id}.png`)
+      }
+    },
 
 
     /**
