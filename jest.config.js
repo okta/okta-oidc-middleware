48c9cd089e632563da086efb0c045e340686047c
Jared Perreault <jared.perreault@okta.com>
2026-08-04 10:23:11 -0400

    deps upgrade

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,6 @@ module.exports = {
     legacyFakeTimers: true
   },
   moduleNameMapper: {
-    '^express$': process.env.EXPRESS5 === '1' ? '<rootDir>/node_modules/express5' : '<rootDir>/node_modules/express4',
-    'uuid': '<rootDir>/node_modules/uuid/dist/index.js'
+    '^express$': process.env.EXPRESS5 === '1' ? '<rootDir>/node_modules/express5' : '<rootDir>/node_modules/express4'
   }
 };
